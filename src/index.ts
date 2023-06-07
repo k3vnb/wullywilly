133d988ec2101d6d1c41ff02525615819d99305f
Kevin B <papershack@gmail.com>
2023-06-07 14:58:33 -0700

    add functionality for erasing drawn objects

--- src/index.ts
+++ src/index.ts
@@ -8,32 +8,72 @@ const imageHeight = imageWidth;
 export const sketch = (p: p5) => {
   let bg: p5.Image | undefined;
   let groupId: string;
+  let isEraseMode = false;
+  let eraseModeToggleButton: p5.Element;
+
+  const shapes: DoodleShape[] = [];
+  const newShapes: DoodleShape[] = [];
 
   p.preload = () => {
     bg = p.loadImage('./assets/woolywilly.svg');
   };
 
-  p.setup = () => {
-    p.createCanvas(imageWidth, imageHeight, p.WEBGL);
+  const drawBackground = () => {
+    p.background(255);
     p.imageMode(p.CENTER);
     if (bg) p.image(bg, 0, 0, imageWidth, imageHeight);
+  };
+
+  p.setup = () => {
+    p.createCanvas(imageWidth, imageHeight, p.WEBGL);
+    drawBackground();
 
     const almostWhite = 230;
     p.fill(almostWhite);
+
+    eraseModeToggleButton = p.createButton('erase').mousePressed(() => {
+      isEraseMode = !isEraseMode;
+      eraseModeToggleButton.html(isEraseMode ? 'draw' : 'erase');
+    });
   };
 
   const drawAtMousePos = () => {
     const [x, y] = getRealMouseCoords({ p, imageHeight, imageWidth });
-    const doodleShape = new DoodleShape({ x, y, groupId, p });
-    doodleShape.display();
+    if (!isEraseMode) {
+      const doodleShape = new DoodleShape({ x, y, groupId, p });
+      newShapes.push(doodleShape);
+      newShapes.forEach((shape) => shape.display());
+    } else {
+      p.loop();
+      shapes.forEach((shape) => {
+        if (shape.isHovered(x, y, p)) shape.setHide(true);
+      });
+    }
+  };
+
+  const removeHiddenShapes = () => {
+    for (let i = shapes.length - 1; i >= 0; i--) {
+      if (shapes[i].isHidden()) shapes.splice(i, 1);
+    }
+  };
+
+  p.mouseDragged = drawAtMousePos;
+  p.mousePressed = () => {
+    groupId = getUid();
+    drawAtMousePos();
+  };
+  p.mouseReleased = () => {
+    if (isEraseMode) removeHiddenShapes();
+    if (newShapes.length) shapes.push(...newShapes);
+    newShapes.length = 0;
   };
 
   p.draw = () => {
-    p.mouseDragged = drawAtMousePos;
-    p.mousePressed = () => {
-      groupId = getUid();
-      drawAtMousePos();
-    };
+    drawBackground();
+    for (let i = 0; i < shapes.length; i++) {
+      if (!shapes[i].isHidden()) shapes[i].display();
+    }
+    p.noLoop();
   };
 };
 
--- src/shapes/DoodleShape.ts
+++ src/shapes/DoodleShape.ts
@@ -1,23 +1,43 @@
+import * as p5 from 'p5';
+
 import { DotAndSpeck } from './DotAndSpeck';
 import { SquigglyQuad } from './SquigglyQuad';
 
 import { getUid } from '../util';
 import type { IDoodleShape } from './types';
 
+const threshold = 20;
 export class DoodleShape {
   id: string;
   groupId: string;
   dotAndSpeck: DotAndSpeck;
   squigglyQuad: SquigglyQuad;
+  x: number;
+  y: number;
+  shouldHide = false;
 
   constructor({ x, y, groupId, p }: IDoodleShape) {
     this.id = getUid();
     this.groupId = groupId;
+    this.x = x;
+    this.y = y;
     this.dotAndSpeck = new DotAndSpeck({ x, y, p });
     this.squigglyQuad = new SquigglyQuad({ x, y, p });
   }
 
+  isHovered = (mouseX: number, mouseY: number, p: p5) => {
+    const d = p.dist(mouseX, mouseY, this.x, this.y);
+    return d < threshold;
+  };
+
+  isHidden = () => this.shouldHide;
+
+  setHide = (hide: boolean) => {
+    this.shouldHide = hide;
+  };
+
   display = () => {
+    if (this.shouldHide) return;
     this.dotAndSpeck.display();
     this.squigglyQuad.display();
   };
